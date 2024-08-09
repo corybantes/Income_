7b0789617fb89c0cb4a1f120ff84748b4e179ff5
corybantes <farukadedeji17@gmail.com>
2024-08-08 19:56:11 -0700

    main

---
+++ src/IncomeExpenseManager/login3.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package IncomeExpenseManager;
+
+/**
+ *
+ * @author User
+ */
+public class login3 {
+    public static String logindetails;
+}
